27d4f973ae66995d4168cb3700955e192365c709
Benjamin Wishart <benjamin.wishart@gmail.com>
2018-04-18 23:51:02 -0400

    Add tables to create script

--- Database/create.sql
+++ Database/create.sql
@@ -1,3 +1,11 @@
--- creaet sql
+\i tables/admin_user.sql
+\i tables/app_user.sql
 
-\i tables/user.sql
+\i tables/following.sql
+\i tables/like.sql
+
+\i tables/review.sql
+\i tables/reviewable_product.sql
+\i tables/reviewable_service.sql
+\i tables/reviewable_business.sql
+\i tables/reviewable_event.sql
