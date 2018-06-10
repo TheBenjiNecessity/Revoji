a0be65337e0f7b68df2db41cc02b13f7e3cc57d0
Benjamin Wishart <benjamin.wishart@gmail.com>
2018-06-09 22:06:18 -0400

    Simplify Reviewable db table/model

--- Database/tables/review.sql
+++ Database/tables/review.sql
@@ -7,15 +7,9 @@ CREATE TABLE review (
     emojis text,
 
     app_user_id int NOT NULL,
-    reviewable_event_id int,
-    reviewable_product_id int,
-    reviewable_service_id int,
-    reviewable_business_id int,
+    reviewable_id int,
 
 	CONSTRAINT review_primary_key PRIMARY KEY (id),
     CONSTRAINT review_user_foreign_key FOREIGN KEY (app_user_id) REFERENCES app_user (id),
-    CONSTRAINT review_reviewable_event_foreign_key FOREIGN KEY (reviewable_event_id) REFERENCES reviewable_event (id),
-    CONSTRAINT review_reviewable_product_foreign_key FOREIGN KEY (reviewable_product_id) REFERENCES reviewable_product (id),
-    CONSTRAINT review_reviewable_service_foreign_key FOREIGN KEY (reviewable_service_id) REFERENCES reviewable_service (id),
-    CONSTRAINT review_reviewable_business_foreign_key FOREIGN KEY (reviewable_business_id) REFERENCES reviewable_business (id)
+    CONSTRAINT review_reviewable_foreign_key FOREIGN KEY (reviewable_id) REFERENCES reviewable_event (id),
 );
---
+++ Database/tables/reviewable.sql
@@ -0,0 +1,19 @@
+--reviewable
+
+CREATE TABLE reviewable (
+    id serial,
+
+    title text NOT NULL,
+    type text NOT NULL,
+    description text NOT NULL,
+
+    content json,
+    info json,
+
+    --info would have:
+    --    UPC Code for products
+    --    Event start/end time
+    --    etc
+
+	CONSTRAINT reviewable_primary_key PRIMARY KEY (id)
+);
