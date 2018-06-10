a102d41a66c31ccf7ddf6a5618029142ed97dcde
Benjamin Wishart <benjamin.wishart@gmail.com>
2018-06-09 22:14:28 -0400

    Move data contexts

---
+++ WebApi/RevojiWebApi/DBTables/DBContexts/RevojiDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RevojiWebApi.DBTables;
+
+namespace RevojiWebApi.DBTables.DBContexts
+{
+    public class RevojiDBContext : DbContext
+    {
+        public RevojiDBContext(DbContextOptions<RevojiDBContext> options) : base(options) {}
+
+        public DbSet<DBAppUser> AppUsers { get; set; }
+        public DbSet<DBAdminUser> AdminUsers { get; set; }
+        public DbSet<DBReview> Reviews { get; set; }
+        public DbSet<DBReviewable> Reviewable { get; set; }
+        public DbSet<DBLike> Likes { get; set; }
+        public DbSet<DBFollowing> Followings { get; set; }
+    }
+}
---
+++ WebApi/RevojiWebApi/DBTables/DBContexts/RevojiDataContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using RevojiWebApi.DBTables;
+
+namespace RevojiWebApi.DBTables.DBContexts
+{
+    public class RevojiDataContext : IDisposable
+    {
+        protected RevojiDBContext context;
+
+        public IQueryable<DBAppUser> AppUsers { get { return context.AppUsers; } }
+        public IQueryable<DBAdminUser> AdminUsers { get { return context.AdminUsers; } }
+        public IQueryable<DBReview> Reviews { get { return context.Reviews; } }
+        public IQueryable<DBReviewable> Reviewable { get { return context.Reviewable; } }
+        public IQueryable<DBFollowing> Followings { get { return context.Followings; } }
+        public IQueryable<DBLike> Likes { get { return context.Likes; } }
+
+
+        public RevojiDataContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<RevojiDBContext>();
+            optionsBuilder.UseNpgsql(AppSettings.ConnectionString);
+            context = new RevojiDBContext(optionsBuilder.Options);
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+
+        public TEntity Get<TEntity>(int id) where TEntity : class
+        {
+            return context.Set<TEntity>().Find(id);
+        }
+
+        public void Add<TEntity>(TEntity entity) where TEntity : class
+        {
+            context.Set<TEntity>().Add(entity);
+        }
+
+        public void Remove<TEntity>(TEntity entity) where TEntity : class
+        {
+            context.Set<TEntity>().Remove(entity);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+    }
+}
