ced396ecbcd2abd9609dcf3a1e1422c98db318d8
TheBenjiNecessity <benjamin.wishart@gmail.com>
2018-06-29 20:48:55 -0400

    Add model builder

--- WebApi/RevojiWebApi/DBTables/DBContexts/RevojiDBContext.cs
+++ WebApi/RevojiWebApi/DBTables/DBContexts/RevojiDBContext.cs
@@ -5,13 +5,29 @@ namespace RevojiWebApi.DBTables.DBContexts
 {
     public class RevojiDBContext : DbContext
     {
-        public RevojiDBContext(DbContextOptions<RevojiDBContext> options) : base(options) {}
-
         public DbSet<DBAppUser> AppUsers { get; set; }
         public DbSet<DBAdminUser> AdminUsers { get; set; }
         public DbSet<DBReview> Reviews { get; set; }
         public DbSet<DBReviewable> Reviewable { get; set; }
         public DbSet<DBLike> Likes { get; set; }
         public DbSet<DBFollowing> Followings { get; set; }
+        
+        public RevojiDBContext(DbContextOptions<RevojiDBContext> options) : base(options) {}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DBFollowing>()
+                        .HasKey(f => new { f.FollowerAppUserId, f.FollowingAppUserId });
+            
+            modelBuilder.Entity<DBFollowing>()
+                        .HasOne(f => f.Follower)
+                        .WithMany(a => a.Followers)
+                        .HasForeignKey(f => f.FollowerAppUserId);
+            
+            modelBuilder.Entity<DBFollowing>()
+                        .HasOne(f => f.Following)
+                        .WithMany(a => a.Followings)
+                        .HasForeignKey(f => f.FollowingAppUserId);
+        }
     }
 }
