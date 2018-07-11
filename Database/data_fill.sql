e385fa3a9d35f0e572149fb390720e6bdc24adc0
Benjamin Wishart <benjamin.wishart@gmail.com>
2018-07-10 22:11:36 -0400

    Add sample data

---
+++ Database/data_fill.sql
@@ -0,0 +1,12 @@
+\copy app_user from './sample_users.csv' delimiter ',' csv header;
+\copy reviewable from './sample_reviewables.csv' delimiter ',' csv header;
+\copy review from './sample_reviews.csv' delimiter ',' csv header;
+\copy follower from './sample_followers.csv' delimiter ',' csv header;
+\copy review_like from './sample_likes.csv' delimiter ',' csv header;
+
+-- init every app_user to have a hashed password of 'password' with same salt
+update app_user set password = '$2b$10$InGuymmSQhNObJbmo0lx/upbu4JFPsPYgmfnugQu71baAlZCXedDi';
+update app_user set salt = '$2b$10$InGuymmSQhNObJbmo0lx/u';
+
+
+--TRUNCATE review_like, follower, review, reviewable, app_user RESTART IDENTITY;
