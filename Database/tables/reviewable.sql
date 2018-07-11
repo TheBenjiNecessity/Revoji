e888cb9c8db5531d64987a205c191a646daa88ff
Benjamin Wishart <benjamin.wishart@gmail.com>
2018-07-10 22:15:57 -0400

    Add title_image_url to reviewable table

--- Database/tables/reviewable.sql
+++ Database/tables/reviewable.sql
@@ -1,19 +1,20 @@
 -- ===== reviewable: a thing to attach a review to =====
 --       title: the main text displayed as a title
 --        type: what kind of reviewable this is
-                    -- time/location dependant (event, concert)
-                    -- consumable (movie, product, food)
-                    -- service ongoing (cell phone plan, tv package)
-                    -- business (restaurant, cell carrier)
+            -- time/location dependant (event, concert)
+            -- consumable (movie, product, food)
+            -- service ongoing (cell phone plan, tv package)
+            -- business (restaurant, cell carrier)
 -- description: a short bio of the reviewable
 --     content: a container for things like imagery (localized)
 --        info: a container for things like:
-                    -- UPC Code for products
-                    -- Event start/end time
+            -- UPC Code for products
+            -- Event start/end time
 CREATE TABLE reviewable (
     id serial,
     title text NOT NULL,
     type text NOT NULL,
+    title_image_url text,
     description text,
     content json,
     info json,
