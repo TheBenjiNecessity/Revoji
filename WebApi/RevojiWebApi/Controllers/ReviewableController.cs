4d85783a8d17c7b8e8f41d9bff6501acd7ad8c7b
TheBenjiNecessity <benjamin.wishart@gmail.com>
2018-07-14 23:53:47 -0400

    List reviewables by type

--- WebApi/RevojiWebApi/Controllers/ReviewableController.cs
+++ WebApi/RevojiWebApi/Controllers/ReviewableController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using RevojiWebApi.DBTables;
@@ -8,7 +9,7 @@ using RevojiWebApi.Models;
 namespace RevojiWebApi.Controllers
 {
     [Route("service-api/[controller]")]
-    public class ReviewableController : Controller
+    public partial class ReviewableController : Controller
     {
         [Authorize]//what about one user being able to access another users stuff? claims?
         [HttpGet("{id}")]
@@ -21,8 +22,49 @@ namespace RevojiWebApi.Controllers
                 {
                     return new NotFoundResult();
                 }
-                return Ok(new Reviewable(dbReviewable));
+                return Ok(new ReviewableDetail(dbReviewable));
+            }
+        }
+
+        [Authorize]
+        [HttpGet("list/type/{type}")]
+        public IActionResult ListByType(string type,
+                                        string order = "DESC",//what would you order?
+                                        int pageStart = 0,
+                                        int pageLimit = 20)
+        {
+            //what would you order?
+            // title (alphabetically), company name (alphabetically), 
+
+            using (var context = new RevojiDataContext())
+            {
+                var reviewables = context.Reviewables.Where(r => r.Type == type);
+
+                IQueryable<DBReviewable> orderedReviews;
+                if (order == "DESC")
+                {
+                    orderedReviews = reviewables.OrderByDescending(r => r.Title)
+                                                .Skip(pageStart)
+                                                .Take(pageLimit);
+                }
+                else if (order == "ASC")
+                {
+                    orderedReviews = reviewables.OrderBy(r => r.Title)
+                                                .Skip(pageStart)
+                                                .Take(pageLimit);
+                }
+                else
+                {
+                    return BadRequest("Bad order direction parameter given. Must be either DESC or ASC.");
+                }
+
+                return Ok(orderedReviews.Select(r => new Reviewable(r)));
             }
         }
     }
+
+    /**
+     * List Reviewables by User interest
+     * List Reviewables by trending
+     */
 }
