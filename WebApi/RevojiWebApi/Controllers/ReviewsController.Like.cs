43cc2289c325e8b397b93780dd5f1c2db4d0e2e2
TheBenjiNecessity <benjamin.wishart@gmail.com>
2018-07-15 21:30:06 -0400

    Add lIke controller

---
+++ WebApi/RevojiWebApi/Controllers/ReviewsController.Like.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RevojiWebApi.DBTables;
+using RevojiWebApi.DBTables.DBContexts;
+using RevojiWebApi.Models;
+
+namespace RevojiWebApi.Controllers
+{
+    public partial class ReviewsController
+    {
+        [Authorize]
+        [HttpPost("like/{id}")]
+        public IActionResult CreateLike([FromBody]Like like)
+        {
+            using (var context = new RevojiDataContext())
+            {
+                DBLike dBLike = new DBLike();
+                like.UpdateDB(dBLike);
+
+                context.Add(dBLike);
+                context.Save();
+
+                return Ok(new Like(dBLike));
+            }
+        }
+
+        [Authorize]
+        [HttpDelete("like/{id}")]
+        public IActionResult DeleteLike(int id)
+        {
+            using (var context = new RevojiDataContext())
+            {
+                DBLike dBLike = context.Get<DBLike>(id);
+                if (dBLike == null)
+                {
+                    return new NotFoundResult();
+                }
+
+                context.Remove(dBLike);
+                context.Save();
+
+                return Ok();
+            }
+        }
+    }
+}
