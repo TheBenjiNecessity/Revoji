0a7309202b8950b71fbdbe580e621e4ba1432c3b
Benjamin Wishart <benjamin.wishart@gmail.com>
2018-08-20 23:22:08 -0400

    Enforce uniqueness for reviews

--- Database/tables/review.sql
+++ Database/tables/review.sql
@@ -16,5 +16,7 @@ CREATE TABLE review (
 
 	CONSTRAINT review_primary_key PRIMARY KEY (id),
     CONSTRAINT review_user_foreign_key FOREIGN KEY (app_user_id) REFERENCES app_user (id),
-    CONSTRAINT review_reviewable_foreign_key FOREIGN KEY (reviewable_id) REFERENCES reviewable (id)
+    CONSTRAINT review_reviewable_foreign_key FOREIGN KEY (reviewable_id) REFERENCES reviewable (id),
+
+    UNIQUE (app_user_id, reviewable_id)
 );
