7b32173f0b929c230ccbe2921c6b959dbbc55b5f
Benjamin Wishart <benjamin.wishart@gmail.com>
2018-08-26 20:35:31 -0400

    Clean up main controller

--- Site/src/app/components/main/main.controller.ts
+++ Site/src/app/components/main/main.controller.ts
@@ -1,13 +1,11 @@
 import * as angular from 'angular';
-import * as $ from 'jquery';
 
 export class MainController {
-    static $inject = ['$http'];
+    static $inject = [];
     test:string = 'tests';
 
-    constructor(private $http) {
+    constructor() {
         this.test = 'It worked!'
-        console.log('test');
 
         let username = 'test';
         let password = 'test';
@@ -20,18 +18,18 @@ export class MainController {
             grant_type: "password"
         };
 
-        this.$http({
-            method: 'POST',
-            url: '//localhost:8000/api/connect/token',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            data: $.param(data)
-        }).then(resp => {
-            console.log(resp.data);
-        }, err => {
-            console.log(err.data);
-        });
+        // this.$http({
+        //     method: 'POST',
+        //     url: '//localhost:8000/api/connect/token',
+        //     headers: {
+        //         'Content-Type': 'application/x-www-form-urlencoded'
+        //     },
+        //     data: $.param(data)
+        // }).then(resp => {
+        //     console.log(resp.data);
+        // }, err => {
+        //     console.log(err.data);
+        // });
     }
 }
 
