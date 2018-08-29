2db496ba7db0931ae25f2b13e5fa26de1ca257b3
TheBenjiNecessity <benjamin.wishart@gmail.com>
2018-08-29 11:21:56 -0400

    Add basic scss styles
    ...also use spaces instead of tabs in webpack config

--- Site/src/config/webpack.config.js
+++ Site/src/config/webpack.config.js
@@ -1,23 +1,33 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-	entry: {
-		app: './src/main.ts'
-	},
-	output: {
-		filename: 'bundle.js'
-	},
-	module: {
+    mode: 'development',
+    entry: {
+        app: './src/main.ts'
+    },
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
         rules: [{
             test: /\.ts$/,
             use: 'ts-loader'
-        },{
-            test: /(components|shared).+\.html$/,
+        }, {
+            test: /(components|shared).+\.html$/,//TODO
             use: { loader: 'html-loader' }
+        }, {
+            test: /\.scss$/,
+            use: [
+                { loader: "style-loader" },
+                { loader: "css-loader" },
+                { loader: "sass-loader" }
+            ]
         }]
-	},
-	resolve: {
-		extensions: [ '.ts', '.js' ]
-	},
-	devServer: {
+    },
+    resolve: {
+        extensions: [ '.ts', '.js', '.scss' ]
+    },
+    devServer: {
         host: '0.0.0.0',
         https: true,
         port: 8000,
--- Site/src/main.ts
+++ Site/src/main.ts
@@ -2,4 +2,6 @@ import * as jQuery from 'jquery';
 
 import './app/app.module';
 
+import './assets/styles/main';
+
 window['$'] = jQuery;
