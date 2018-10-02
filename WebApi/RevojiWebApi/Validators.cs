8a596224e2a9687b63446d8ca228c81db6fbb158
TheBenjiNecessity <benjamin.wishart@gmail.com>
2018-10-01 21:25:37 -0400

    Add more validation for password

--- WebApi/RevojiWebApi/Validators.cs
+++ WebApi/RevojiWebApi/Validators.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using IdentityServer4.Models;
 using IdentityServer4.Validation;
 using RevojiWebApi.DBTables;
 using RevojiWebApi.DBTables.DBContexts;
@@ -11,22 +12,29 @@ namespace RevojiWebApi
 {
 	public class ResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
     {
-		Task IResourceOwnerPasswordValidator.ValidateAsync(ResourceOwnerPasswordValidationContext context)
-		{
-			using (var dbctx = new RevojiDataContext())
-			{
-				//TODO: check if username or password is null
+        Task IResourceOwnerPasswordValidator.ValidateAsync(ResourceOwnerPasswordValidationContext context)
+        {
+            using (var dbctx = new RevojiDataContext())
+            {
+                if (context.UserName == null || context.Password == null)
+                {
+                    context.Result = new GrantValidationResult(TokenRequestErrors.InvalidRequest, "missing_arguments_error");
+                    return Task.FromResult<object>(null);
+                }
 
-				DBAppUser dbAppUser = dbctx.AppUsers.FirstOrDefault(u => u.Handle == context.UserName);
-				if (dbAppUser != null && dbAppUser.isPasswordCorrect(context.Password))
-				{
-					var claim = new Claim(ClaimTypes.Name, dbAppUser.Handle);
-					var claims = new List<Claim>();
-					claims.Add(claim);
-					context.Result = new GrantValidationResult(dbAppUser.Handle, "access_token", claims);
-				}
+                DBAppUser dbAppUser = dbctx.AppUsers.FirstOrDefault(u => u.Handle == context.UserName);
+                if (dbAppUser == null || !dbAppUser.isPasswordCorrect(context.Password))
+                {
+                    context.Result = new GrantValidationResult(TokenRequestErrors.InvalidGrant, "invalid_handle_password_error");
+                    return Task.FromResult<object>(null);
+                }
+
+                var claim = new Claim(ClaimTypes.Name, dbAppUser.Handle);
+                var claims = new List<Claim>();
+                claims.Add(claim);
+                context.Result = new GrantValidationResult(dbAppUser.Handle, "access_token", claims);
+                return Task.FromResult<object>(null);
 			}
-			return Task.FromResult<object>(null);
 		}
 	}
 }
