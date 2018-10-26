6a6390d713a8cee852e923ee49f64345980a26b6
TheBenjiNecessity <benjamin.wishart@gmail.com>
2018-10-25 23:11:11 -0400

    Add more fake data to database

--- Database/data_fill.sql
+++ Database/data_fill.sql
@@ -5,8 +5,8 @@
 \copy review_like from './sample_likes.csv' delimiter ',' csv header;
 
 -- init every app_user to have a hashed password of 'password' with same salt
-update app_user set password = '$2b$10$InGuymmSQhNObJbmo0lx/upbu4JFPsPYgmfnugQu71baAlZCXedDi';
-update app_user set salt = '$2b$10$InGuymmSQhNObJbmo0lx/u';
+--update app_user set password = '$2b$10$InGuymmSQhNObJbmo0lx/upbu4JFPsPYgmfnugQu71baAlZCXedDi';
+--update app_user set salt = '$2b$10$InGuymmSQhNObJbmo0lx/u';
 
 -- If you want to completely obliterate your database and start from scratch
---TRUNCATE review_like, follower, review, reviewable, app_user RESTART IDENTITY;
+-- TRUNCATE review_like, follower, review, reviewable, app_user RESTART IDENTITY;
