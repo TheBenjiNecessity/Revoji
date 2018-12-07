b4268bb374cdadaff1877c337329ce6428974f80
TheBenjiNecessity <benjamin.wishart@gmail.com>
2018-12-06 21:32:55 -0500

    Calculate emoji counts

--- WebApi/RevojiWebApi/Models/ReviewableDetail.cs
+++ WebApi/RevojiWebApi/Models/ReviewableDetail.cs
@@ -1,22 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using RevojiWebApi.DBTables;
+using RevojiWebApi.DBTables.DBContexts;
 
 namespace RevojiWebApi.Models
 {
 	public class ReviewableDetail : Reviewable
     {
-		dynamic Content;
+        public int reviewCount { get; set; }
 
-		dynamic Info;
+        public Dictionary<string, int> emojiCounts { get; set; }
 
-		public ReviewableDetail() { }
+        dynamic Content;
+
+        dynamic Info;
+
+        public ReviewableDetail() { }
 
 		public ReviewableDetail(DBReviewable dBReviewable) : base(dBReviewable)
 		{
 			Content = dBReviewable.Content;
 			Info = dBReviewable.Info;
-		}
+
+            using (var context = new RevojiDataContext())
+            {
+                var reviews = context.Reviews.Where(r => r.ReviewableId == ID);
+                reviewCount = reviews.Count();
+
+                if (reviewCount == 0)
+                {
+                    emojiCounts = null;
+                }
+
+                string emojis = new string(reviews.SelectMany(r => r.Emojis)
+                                                  .ToArray());
+
+                emojiCounts = emojis.Split(",")
+                                    .GroupBy(e => e, StringComparer.OrdinalIgnoreCase)
+                                    .ToDictionary(group => group.Key, group => group.Count());
+            }
+        }
 
 		public override void UpdateDB(DBTable dbModel)
 		{
