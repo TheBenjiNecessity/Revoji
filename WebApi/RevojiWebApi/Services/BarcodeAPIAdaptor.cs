fb7d78e2e785cb63fff80fedbbe4ec5c93faf211
TheBenjiNecessity <benjamin.wishart@gmail.com>
2019-01-07 22:15:23 -0500

    Make barcode adaptor work

--- WebApi/RevojiWebApi/Services/BarcodeAPIAdaptor.cs
+++ WebApi/RevojiWebApi/Services/BarcodeAPIAdaptor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -9,9 +10,11 @@ namespace RevojiWebApi.Services
 {
     public class BarcodeAPIAdaptor : ReviewableAPIAdaptor
     {
+        public static string TPNAME = "barcode";
+
         public BarcodeAPIAdaptor()
         {
-            apiKey = "";
+            apiKey = "lnh8fwquw4yk7yaaxszoa09xhpahvs";
             apiUrl = "https://api.barcodelookup.com/v2/products?key=" + apiKey;
         }
 
@@ -32,41 +35,83 @@ namespace RevojiWebApi.Services
             if (response.IsSuccessStatusCode)
             {
                 var reviewableString = await response.Content.ReadAsStringAsync();
-                BarcodeJSONResponse reviewableJSON = JsonConvert.DeserializeObject<BarcodeJSONResponse>(reviewableString);
-
-                reviewable = new Reviewable();
-                reviewable.Title = reviewableJSON.title;
-                reviewable.TitleImageUrl = reviewableJSON.images[0];
+                BarcodeJSONSearchResponse reviewableJSON = JsonConvert.DeserializeObject<BarcodeJSONSearchResponse>(reviewableString);
+                var product = reviewableJSON.reviewables.First();
+                reviewable = getReviewable(product);
             }
             return reviewable;
         }
 
         public override async Task<Reviewable[]> SearchReviewablesAsync(string searchText, int pageOffset, int pageLimit)
         {
-            Reviewable reviewable = null;
+            Reviewable[] reviewables = null;
             HttpResponseMessage response = await client.GetAsync(getUrlForSearch(searchText, pageOffset, pageLimit));
+
             if (response.IsSuccessStatusCode)
             {
                 var reviewableString = await response.Content.ReadAsStringAsync();
-                List<BarcodeJSONResponse> reviewableJSON = JsonConvert.DeserializeObject<List<BarcodeJSONResponse>>(reviewableString);
+                BarcodeJSONSearchResponse search = JsonConvert.DeserializeObject<BarcodeJSONSearchResponse>(reviewableString);
+                reviewables = search.reviewables.Select(getReviewable).ToArray();
             }
-            return null;
+
+            return reviewables;
+        }
+
+        private Reviewable getReviewable(BarcodeJSONResponse product)
+        {
+            var reviewable = new Reviewable();
+            reviewable.Title = product.product_name;
+            reviewable.TitleImageUrl = product.images.First();
+            reviewable.TpId = product.barcode_number;
+            reviewable.TpName = TPNAME;
+            reviewable.Type = product.category.Split('>').Last().Trim();
+            reviewable.Description = product.description;
+            return reviewable;
         }
     }
 
     public class BarcodeJSONResponse
     {
+        [JsonProperty("barcode_number")]
         public String barcode_number { get; set; }
+
+        [JsonProperty("model")]
         public String model { get; set; }
+
+        [JsonProperty("product_name")]
         public String product_name { get; set; }
+
+        [JsonProperty("title")]
         public String title { get; set; }
+
+        [JsonProperty("category")]
         public String category { get; set; }
+
+        [JsonProperty("manufacturer")]
         public String manufacturer { get; set; }
+
+        [JsonProperty("brand")]
         public String brand { get; set; }
+
+        [JsonProperty("label")]
         public String label { get; set; }
+
+        [JsonProperty("artist")]
         public String artist { get; set; }
+
+        [JsonProperty("genre")]
         public String genre { get; set; }
+
+        [JsonProperty("description")]
         public String description { get; set; }
+
+        [JsonProperty("images")]
         public IList<string> images { get; set; }
+    }
+
+    public class BarcodeJSONSearchResponse
+    {
+        [JsonProperty("products")]
+        public List<BarcodeJSONResponse> reviewables { get; set; }
     }
 }
