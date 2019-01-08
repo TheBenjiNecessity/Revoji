1a7d04adef2ca54e5fbd585ea69ab4a57546c99e
TheBenjiNecessity <benjamin.wishart@gmail.com>
2019-01-07 22:12:47 -0500

    Add dev urls

--- WebApi/RevojiWebApi/Program.cs
+++ WebApi/RevojiWebApi/Program.cs
@@ -19,7 +19,8 @@ namespace RevojiWebApi
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+                   .UseStartup<Startup>()
+                   .UseUrls("http://localhost:5001", "http://10.0.1.23:5001")
+                   .Build();
     }
 }
