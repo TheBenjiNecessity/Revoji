3255d4e52d0d1b38fea06f0719ed3a3c18e88c74
TheBenjiNecessity <benjamin.wishart@gmail.com>
2019-05-06 21:15:03 -0400

    Add appuserprefs

---
+++ WebApi/RevojiWebApi/DBTables/JSONObjects/AppUserPreferences.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace RevojiWebApi.DBTables.JSONObjects
+{
+    public class AppUserPreferences
+    {
+        public string PoliticalAffiliation { get; set; }
+        public string PoliticalOpinion { get; set; }
+        public string Location { get; set; }
+        public string Religiosity { get; set; }
+        public string Personality { get; set; }
+    }
+}
