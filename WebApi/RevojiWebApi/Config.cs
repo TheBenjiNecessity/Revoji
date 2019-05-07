e41a843b5fa7fa7c1bdf102e17cf63074f1dcabc
TheBenjiNecessity <benjamin.wishart@gmail.com>
2019-05-06 21:17:00 -0400

    Set access token lifetime to one year

--- WebApi/RevojiWebApi/Config.cs
+++ WebApi/RevojiWebApi/Config.cs
@@ -6,32 +6,33 @@ using IdentityServer4.Models;
 namespace RevojiWebApi
 {
     public class Config
-	{
-		public static IEnumerable<ApiResource> GetApiResources()
-		{
-			return new List<ApiResource>
-			{
-				new ApiResource("api", "Revoji Api")
-			};
-		}
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api", "Revoji Api")
+            };
+        }
 
-		public static IEnumerable<Client> GetClients()
-		{
-			return new List<Client>
-			{
-				new Client
-				{
-					ClientId = "com.revoji",
-					ClientSecrets =
-					{
-						new Secret("secret".Sha256())
-					},
-					AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
-					AllowAccessTokensViaBrowser = true,
-					AllowedScopes = { "api" },
-					AllowedCorsOrigins = { "https://127.0.0.1:8000" },//TODO needs to be in config
-				}
-			};
-		}
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "com.revoji",
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    AllowAccessTokensViaBrowser = true,
+                    AccessTokenLifetime = 31536000,
+                    AllowedScopes = { "api" },
+                    AllowedCorsOrigins = { "http://localhost:3000" }//TODO needs to be in config
+                }
+            };
+        }
     }
 }
