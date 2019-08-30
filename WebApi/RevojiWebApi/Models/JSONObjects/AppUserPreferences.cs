07a5b94bf88a71f3827b1187a8302fe44b25d78a
TheBenjiNecessity <benjamin.wishart@gmail.com>
2019-08-29 22:12:26 -0400

    Change the way preferences work

--- WebApi/RevojiWebApi/Models/JSONObjects/AppUserPreferences.cs
+++ WebApi/RevojiWebApi/Models/JSONObjects/AppUserPreferences.cs
@@ -1,13 +1,21 @@
 ﻿using System;
+using System.Linq;
+
 namespace RevojiWebApi.Models
 {
     public class AppUserPreferences
     {
-        public string AgeRange { get; set; }
+        public int AgeRangeMin { get; set; }
+        public int AgeRangeMax { get; set; }
         public float PoliticalAffiliation { get; set; }
         public float PoliticalOpinion { get; set; }
-        public string Location { get; set; }
+        public string[] Location { get; set; }
         public float Religiosity { get; set; }
         public float Personality { get; set; }
+
+        public bool locationsMatch(string[] locations)
+        {
+            return Location.Intersect(locations).Any();
+        }
     }
 }
