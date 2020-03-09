ce2b33f335e9caa90cb84f8694645aa328dd6ba8
TheBenjiNecessity <benjamin.wishart@gmail.com>
2020-03-08 21:15:05 -0400

    Add paged response model

---
+++ WebApi/RevojiWebApi/Models/PagedResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace RevojiWebApi.Models
+{
+    public class PagedResponse <T>
+    {
+        public T[] data;
+        public PageMetaData meta;
+
+        public PagedResponse(T[] data, PageMetaData meta)
+        {
+            this.data = data;
+            this.meta = meta;
+        }
+    }
+
+    public class PageMetaData
+    {
+        public PageMetaData() {}
+    }
+}
